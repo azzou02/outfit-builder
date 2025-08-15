715d4a2d2b4bc75cfa16abbc511484ed3471f8ff
Alison <alisonzou02@gmail.com>
2025-08-14 23:25:31 -0400

    minor changes to names

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -3,7 +3,7 @@ export const clothes = [
     { id: 2, name: "gingham cami", type: "top", src: "/clothes/gingham-tank.png", favorite: false },
     { id: 3, name: "grey longsleeve", type: "top", src: "/clothes/grey-longsleeve.png", favorite: false },
     { id: 4, name: "blue striped longsleeve", type: "top", src: "/clothes/blue-striped-longsleeve.png", favorite: false },
-    { id: 5, name: "creamy blouse cami", type: "top", src: "/clothes/white-blouse.png", favorite: false },
+    { id: 5, name: "creamy blouse cami", type: "top", src: "/clothes/cream-blouse.png", favorite: false },
     { id: 6, name: "navy off shoulder tee", type: "top", src: "/clothes/off-shoulder-top.png", favorite: false },
     { id: 7, name: "grey cardigan", type: "outerwear", src: "/clothes/grey-cardigan.png", favorite: false },
     { id: 8, name: "beige skirt", type: "bottom", src: "/clothes/beige-skirt.png", favorite: false },
